1f17fab2efe4224800062e6cf88bf9b5d89f15e0
MahmoudMohajer <mahmohajer3@gmail.com>
2022-11-18 12:20:01 +0430

    working on templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Mahmoud Mohajer',
+        'title': 'Blog post 1',
+        'content': 'first post content',
+        'date_posted': 'Nov 10 2022'
+    },
+    {
+    'author': 'Saeed',
+    'title': 'Blog post 2',
+    'content': 'second post content',
+    'date_posted': 'Nov 18 2022'
+    }
+
+]
+
+
+
+
+
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return "<h1>Home page</h1>"
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-    return "<h1>about page.</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
